9183a00628d561610044a4c3295e0e2cddae2d16
Meir Gottlieb <meirgottlieb@gmail.com>
2015-09-10 16:09:14 -0600

    Exposed Referenced.areEqual

--- src/hydrate.ts
+++ src/hydrate.ts
@@ -2,3 +2,15 @@ export import Configuration = require("./config/configuration");
 export import NamingStrategies = require("./config/namingStrategies");
 export import ObjectIdGenerator = require("./id/objectIdGenerator");
 export import AnnotationMappingProvider = require("./mapping/providers/annotationMappingProvider");
+
+import Reference = require("./reference");
+
+/**
+ * Returns true if values are equivalent. Values can be entities or references to entities.
+ * @param value1 The first reference or entity to compare.
+ * @param value2 The second reference or entity to compare.
+ */
+export function areEqual(value1: any, value2: any): boolean {
+
+    return Reference.areEqual(value1, value2);
+}
